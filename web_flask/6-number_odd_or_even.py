f0d21ed15f3d6e1de20eb0925c0a15137f284203
Tamene Gelaye <tamenes21@gmail.com>
2023-04-23 19:51:14 +0000

    if statement corrected

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -47,11 +47,12 @@ def numbersandtemplates(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def evennessnumber(n):
     """Display an HTML page if n is an integer"""
-    if n % 2 == 0;
+    if n % 2 == 0:
         evenness = 'even'
     else:
         evenness = 'odd'
-    return render_template('6-number_odd_or_even.html', n=n, evenness=evenness)
+    return render_template('6-number_odd_or_even.html', n=n,
+                           evenness=evenness)
 
 
 if __name__ == '__main__':
