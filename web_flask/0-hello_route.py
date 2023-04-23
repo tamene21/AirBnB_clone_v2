f36e0ae53915023d2cf93bdf96c6b9915ab6c047
Tamene Gelaye <tamenes21@gmail.com>
2023-04-23 13:09:50 +0000

    The script becames executatble

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
 New Flask application
-
 """
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/',strict_slashes=False)
 def newApp():
-    """Return Hello HBNB!"""
+    """return Hello HBNB!"""
     return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
