641921f298d65f509800efc20a61f2ceacf65b3e
Tamene Gelaye <tamenes21@gmail.com>
2023-04-23 18:37:01 +0000

    check if the input is an integer number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """return Hello HBNB!"""
+     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return HBNB"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cfun(text):
+    """display 'C', followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display 'Python' followed by the variable text"""
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number/<int>', strict_slashes=False)
+def isitnumber(n):
+    """display 'n' if n is an integer number"""
+    return "{:d} is a number".format(n)
+
+
+if __name__==__'main__':
+app.run(host='0.0.0.0', port='5000')
