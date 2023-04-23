deeba21330130169a0dd7a01f4b5658b8da1e67e
Tamene Gelaye <tamenes21@gmail.com>
2023-04-23 19:55:04 +0000

    function parameter corrected

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -51,9 +51,7 @@ def evennessnumber(n):
         evenness = 'even'
     else:
         evenness = 'odd'
-    return render_template('6-number_odd_or_even.html', n=n,
-                           evenness=evenness)
-
+    return render_template('6-number_odd_or_even.html', n=n, evenness=evenness)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
