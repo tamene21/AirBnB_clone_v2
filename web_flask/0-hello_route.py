1eb7f83939d532c93ed4ad10f52876564013f499
Tamene Gelaye <tamenes21@gmail.com>
2023-04-23 16:54:03 +0000

    Formating for pep8 changed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-New Flask application
+starts a Flask web application
 """
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/',strict_slashes=False)
-def newApp():
-    """return Hello HBNB!"""
+def index():
+    """ return Hello HBNB!"""
     return "Hello HBNB!"
 
 if __name__ == '__main__':
