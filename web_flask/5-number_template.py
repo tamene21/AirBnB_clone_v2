50e6d4d840ea1335eb36f49c01be2482440363cd
Tamene Gelaye <tamenes21@gmail.com>
2023-04-23 19:23:23 +0000

    pep8 styelec correction

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,8 +39,8 @@ def isitnumber(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbertemplate(n):
-    """display an HTML page while n is integer"""
+def numbersandtemplates(n):
+    """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
