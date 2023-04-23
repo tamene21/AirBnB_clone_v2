4190999387f6a087fe255e844526c0088718e95e
Tamene Gelaye <tamenes21@gmail.com>
2023-04-23 18:01:22 +0000

    host address corrected

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -15,20 +15,20 @@ def index():
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """retunr HBNB"""
+    """return HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cfun(text):
-    """display "C" followed by the variable"""
+    """display "C " followed by the variable"""
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def pythonIsCool(text= 'is cool'):
     """display 'python' followed by the varibale text"""
     return 'python ' + text.replace('_', ' ')
 
-if __name__ == __main__:
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
