6416e881595833a28bc2e36da0f92e3560364d61
Tamene Gelaye <tamenes21@gmail.com>
2023-05-04 14:09:50 +0000

    html file created

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 # Prepare Web Server
 apt-get update -y
+# install nginx if not install
 apt-get install nginx -y
+# create a folder
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
+# creating an html file
+echo '<html>
+ <head>
+ </head>
+ <body>
+    Holberton School
+  </body>
+</html>' > /data/web_static/releases/test/index.html
+# creating a link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+#give ownership of the daa
+chown -hR ubuntu:ubuntu /data/
 
 loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n"
 
